b3d086cc3ede6ae943b41e02fbba1604f3f88bf5
LzyBooy <x5735350@gmail.com>
2025-10-06 18:40:29 +0800

    20251006

--- bmi.js
+++ bmi.js
@@ -15,12 +15,12 @@ const readline = require("readline-sync")
 // }
 // console.log('\nA value the user requested: ' + value);
 
-var weight=0;
+var weight=0; /* 預設體重是0 */
 while(weight<10 || weight>120){
     weight=readline.questionInt("Ur weight? (10~120KG)");
-}
-var height=readline.questionInt("Ur height? (cm)",{limit:'$<70-200>'});
-var bmi=weight/((height/100)**2)
+} /* 這段會一直重複問，直到使用者輸入的體重在 10 ~ 120 公斤之間才會停止。 */
+var height=readline.questionInt("Ur height? (cm)",{limit:'$<70-200>'}); /* 問使用者身高（單位 cm）*/  /*{limit:'$<70-200>'} 是輸入限制：只能輸入 70 ~ 200 公分。*/
+var bmi=weight/((height/100)**2)/*計算BMI的方程式*/
 
 
-console.log("Hello! Your BMI is:" + bmi)
+console.log("Hello! Your BMI is:" + bmi)/*把計算好的 BMI 顯示出來*/
